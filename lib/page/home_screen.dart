575b721b016e7f1b6fc641f2e1cfbd48db4e2219
hwangsoohyeon <mikaelh1651@gmail.com>
2024-12-11 08:41:04 +0900

    chore: change from stless to stful

--- lib/page/home_screen.dart
+++ lib/page/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 import 'package:resume/asset/gen/assets.gen.dart';
 import 'package:resume/util/extension/build_context_extension.dart';
