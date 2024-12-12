eeb0bbefd501b5393d02db15227cb74427e6f69b
hwangsoohyeon <mikaelh1651@gmail.com>
2024-12-12 11:53:34 +0900

    feat: add new texts

--- lib/l10n/gen_l10n/app_localizations_en.dart
+++ lib/l10n/gen_l10n/app_localizations_en.dart
@@ -133,7 +133,7 @@ class AppLocalizationsEn extends AppLocalizations {
   String get coverLetterTitle1 => 'I believe the power of passion';
 
   @override
-  String get coverLetterTitle2 => '\bIf there is no passion to me who is non-major in CS, I couldn\'t become a developer';
+  String get coverLetterTitle2 => 'If there is no passion to me who is non-major in CS, I couldn\'t become a developer';
 
   @override
   String get coverLetterTitle3 => 'From JAVA backend developer to Flutter developer,';
@@ -176,4 +176,322 @@ class AppLocalizationsEn extends AppLocalizations {
 
   @override
   String get coverLetterCard4_3 => 'From planning to implementation, I consider the customer\'s needs throughout every stage of the process.';
+
+  @override
+  String get skillsLanguageLevel1 => 'Capable of understanding grammar';
+
+  @override
+  String get skillsLanguageLevel2 => 'Capable of CRUD';
+
+  @override
+  String get skillsLanguageLevel3 => 'Capable of building toy projects';
+
+  @override
+  String get skillsLanguageLevel4 => 'Capable of writing test codes';
+
+  @override
+  String get skillsLanguageLevel5 => 'Capable of developing and maintaining projects';
+
+  @override
+  String get skillsLibraryRiverpod1 => 'Able to manage state and consider the dispose timing of instances.';
+
+  @override
+  String get skillsLibraryRiverpod2 => 'Able to manage the state of specific widgets only.';
+
+  @override
+  String get skillsLibraryRetrofit => 'Able to define repositories using the given REST API.';
+
+  @override
+  String get skillsLibraryDio => 'Able to define interceptors to handle API requests, responses, and errors.';
+
+  @override
+  String get skillsLibraryFirebase1 => 'Able to apply push notifications and local notifications.';
+
+  @override
+  String get skillsLibraryFirebase2 => 'Able to use Firebase Storage.';
+
+  @override
+  String get skillsLibraryStorage => 'Able to store and use app-required data based on whether data security is necessary.';
+
+  @override
+  String get skillsLibraryGraphQL => 'Able to request data using given GraphQL queries.';
+
+  @override
+  String get skillsLibraryLogger => 'Able to perform logging tasks for debugging.';
+
+  @override
+  String get skillsLibraryImagePicker => 'Able to import images from the device gallery into the app.';
+
+  @override
+  String get skillsLibrarySyncfusionChart => 'Able to implement charts in the desired format.';
+
+  @override
+  String get skillsLibraryFreezed => 'Able to define both general and generic models.';
+
+  @override
+  String get skillsLibraryWidgetbook => 'Able to preview implemented UI on the web before deployment.';
+
+  @override
+  String get skillsLibraryGoRouter1 => 'Able to apply routing between pages.';
+
+  @override
+  String get skillsLibraryGoRouter2 => 'Able to redirect to specific pages based on data state.';
+
+  @override
+  String get skillsLibrarySkeletonizer => 'Able to implement loading UI.';
+
+  @override
+  String get skillsLibraryLocalization1 => 'Able to implement a language-switching feature.';
+
+  @override
+  String get skillsLibraryLocalization2 => 'Able to update the app language immediately without restarting using state management.';
+
+  @override
+  String get skillsLibraryLocalization3 => 'Able to manage multiple languages per country using Google Spreadsheets.';
+
+  @override
+  String get skillsLibrarySQFLite => 'Able to create and control a local database.';
+
+  @override
+  String get skillsLibraryFlutterGen => 'Able to generate code of required assets.';
+
+  @override
+  String get skillsTechniquePagination1 => 'Able to implement infinite scroll pagination.';
+
+  @override
+  String get skillsTechniquePagination2 => 'Able to apply the throttling technique to prevent duplicate requests.';
+
+  @override
+  String get skillsTechniqueOptimisticResponse => 'Able to improve UX by displaying expected responses first under the assumption of smooth network';
+
+  @override
+  String get skillsTechniqueSorting => 'Able to reduce server load by applying sorting logic within the app.';
+
+  @override
+  String get skillsTechniqueAnimation => 'Able to apply desired animations to widgets.';
+
+  @override
+  String get skillsTechniqueSliver => 'Able to utilize NestedScrollView by combining Sliver Widgets and Box Widgets.';
+
+  @override
+  String get skillsTechniqueReorder => 'Able to implement functionality by combining ReorderableListView and Dismissible widgets.';
+
+  @override
+  String get skillsTechniqueCodeGeneration => 'Able to use features of libraries that support code generation.';
+
+  @override
+  String get skillsCoWork => 'Collaboration tools I have experience with';
+
+  @override
+  String get skillsIde => 'The IDEs I mainly use';
+
+  @override
+  String get skillsLanguage => 'Languages I can use';
+
+  @override
+  String get cvDirectional => 'Directional';
+
+  @override
+  String get cvInvestar => 'At the time of joining, the app was live. I contributed to new page development and maintenance, accounting for approximately 60% of the overall development.';
+
+  @override
+  String get cvInvestarDailyCoin1 => 'Daily Coin Analysis Page Development';
+
+  @override
+  String get cvInvestarDailyCoin2 => 'Allows accessing once per day, with a bottom sheet appearing on the home screen if it is available.';
+
+  @override
+  String get cvInvestarDailyCoin3 => 'Tapping \"View Now\" navigates to the query page, where the daily coin is cached via a provider during the animation.';
+
+  @override
+  String get cvInvestarDailyCoin4 => 'If data retrieval fails due to network issues, retry functionality is available.';
+
+  @override
+  String get cvInvestarDailyCoin5 => 'Once the daily coin is successfully cached, it proceeds to the analysis page.';
+
+  @override
+  String get cvInvestarRisingScore1 => 'Don’t Miss the 30 Minutes Page Development';
+
+  @override
+  String get cvInvestarRisingScore2 => 'This page updates data every 30 minutes. The server provides valid time data, and a timer reduces time every second.';
+
+  @override
+  String get cvInvestarRisingScore3 => 'Except in cases where the app is restarted, the valid time is not re-fetched.';
+
+  @override
+  String get cvInvestarRisingScore4 => 'When the remaining time reaches zero, an expiration screen is displayed, and the app starts requesting new valid times every second.';
+
+  @override
+  String get cvInvestarRisingScore5 => 'Upon receiving updated valid time data, the provider caches the new data.';
+
+  @override
+  String get cvInvestarCarousel1 => 'Home Screen Carousel Banner Development';
+
+  @override
+  String get cvInvestarCarousel2 => 'At the time of building the home screen, providers for carousel-related pages are generated, and data is cached.';
+
+  @override
+  String get cvInvestarCarousel3 => 'If cache data for a specific page is missing, the banner related that page is not rendered.';
+
+  @override
+  String get cvInvestarWhale1 => 'Info Tab – Whale Page Development';
+
+  @override
+  String get cvInvestarWhale2 => 'Displays on-chain data for major cryptocurrencies. Tapping a card navigates to a detail screen with charts.';
+
+  @override
+  String get cvInvestarWhale3 => 'The page initially caches data for BTC, ETH, USDT, and USDC.';
+
+  @override
+  String get cvInvestarWhale4 => 'Developed a coin search function for the \"Most Purchased Coins\" page accessible via a banner.';
+
+  @override
+  String get cvInvestarFavorite1 => 'Favorites Tab – Favorites Coin Page Improvement and Development';
+
+  @override
+  String get cvInvestarFavorite2 => 'Since the number of favorite coins is limited, all data is fetched at once and sorted using frontend logic.';
+
+  @override
+  String get cvInvestarCommunity1 => 'Community Tab – TokTok Page Improvement and Development';
+
+  @override
+  String get cvInvestarCommunity2 => 'Improved the existing feature, which previously allowed only text input, to support image uploads.';
+
+  @override
+  String get cvInvestarCommunity3 => 'Additionally, developed a back-office system for community management and iOS review approval.';
+
+  @override
+  String get cvInvestarMy1 => 'My Tab – Comprehensive Improvement and Development';
+
+  @override
+  String get cvInvestarMy2 => 'Unified fragmented GraphQL queries related to My Page to ensure consistent data management.';
+
+  @override
+  String get cvInvestarMy3 => 'Developed a new adaptive UI to visually display user information.';
+
+  @override
+  String get cvInvestarSearch1 => 'Coin Search Page Improvement and Development';
+
+  @override
+  String get cvInvestarSearch2 => 'Fixed persistent RenderFlex Overflow issues in debug mode.';
+
+  @override
+  String get cvInvestarSearch3 => 'Applied a new adaptive UI.';
+
+  @override
+  String get cvInvestarGlobal1 => 'Implemented Korean/English localization for global release.';
+
+  @override
+  String get cvInvestarGlobal2 => 'Adjusted the overall UI for global deployment';
+
+  @override
+  String get cvInvestarAll => 'Developed generalized widgets and applied automation functions (e.g. Json Serialization,  common components. etc.)';
+
+  @override
+  String get cvSbl => 'Applied libraries and technologies not used in the previous app, contributing approximately 90% to the overall development.';
+
+  @override
+  String get cvSbl1 => 'Utilized the latest Flutter libraries like Riverpod, Retrofit, and Go Router.';
+
+  @override
+  String get cvSbl2 => 'Actively used code-generation libraries.';
+
+  @override
+  String get cvSbl3 => 'Applied the MVVM architectural pattern.';
+
+  @override
+  String get cvSbl4 => 'Developed a redirection feature based on login state.';
+
+  @override
+  String get cvSbl5 => 'Implemented Custom Interceptor using the Dio library to handle requests, responses, and errors effectively.';
+
+  @override
+  String get cvSbl6 => 'Managed state using Riverpod, ensuring proper instance maintenance or disposal based on context.';
+
+  @override
+  String get cvSbl7 => 'Defined generic models using Freezed and generalized infinite scroll pagination screens.';
+
+  @override
+  String get cvSbl8 => 'Prevented redundant requests by applying Throttle and Debouncer techniques.';
+
+  @override
+  String get cvInvestarBackOffice1 => 'Developed a Flutter web-based back office using SBL app technology (100% contribution).';
+
+  @override
+  String get cvInvestarBackOffice2 => 'Focused on community post management for iOS review approval.';
+
+  @override
+  String get cvFireAnt1 => 'Developed using the SBL APP platform as a base. Moved the existing implementation pages to the Short Sale tab and converted them into a new functional app. (100% contribution).';
+
+  @override
+  String get cvFireAnt2 => 'Developed generalized common widgets as per Figma specifications, segmenting list components into leading, content, and trailing formats.';
+
+  @override
+  String get cvFireAnt3 => 'Developed a CustomAnimatedInkWell widget to add animated touch effects to tappable areas like icons and list tiles.';
+
+  @override
+  String get cvFireAnt4 => 'Implemented hiding of the bottom navigation bar and app bar during scrolling using NestedScrollView.';
+
+  @override
+  String get cvFireAnt5 => 'Combined ReorderableListView and Dismissible widgets to allow list deletion by swiping and reordering by long-pressing within the same screen.';
+
+  @override
+  String get cvFireAnt6 => 'Applied a local database library to store all short-selling items at app startup.';
+
+  @override
+  String get cvFireAnt7 => 'Enabled light/dark mode switching without restarting the app using Riverpod for theme management.';
+
+  @override
+  String get cvFireAnt8 => 'Used Google Sheets and Flutter localization libraries to manage app languages dynamically, enabling real-time switching without restarting the app.';
+
+  @override
+  String get cvFireAnt9 => 'Developed a DefaultLayout widget for implementing commonly required functionalities across all pages, such as the Android back button.';
+
+  @override
+  String get cvFireAnt10 => 'Added a highlight feature for matched text when searching items.';
+
+  @override
+  String get cvFireAnt11 => 'Generalized line chart widgets for app usage.';
+
+  @override
+  String get cvFireAnt12 => 'Performed logging tasks to easily verify API requests, responses, and errors.';
+
+  @override
+  String get cvFireAnt13 => 'Developed an automatic asset code generation feature for app images.';
+
+  @override
+  String get cvFireAnt14 => 'Explicitly defined loading, error, empty, and success cases for data-request-related widgets.';
+
+  @override
+  String get cvFireAnt15 => 'Applied Firebase Push Notification and Local Notification functionalities.';
+
+  @override
+  String get cvFireAnt16 => 'Integrated Widgetbook to test and preview UI on the web before deployment.';
+
+  @override
+  String get cvFireAnt17 => 'Developed a balloon-style UI using CustomClipper with calculated coordinates.';
+
+  @override
+  String get cvHectoFinancial1 => 'Hecto Financial';
+
+  @override
+  String get cvHectoFinancial2 => 'Developed and maintained a municipal tax virtual account payment service.';
+
+  @override
+  String get cvHectoFinancial3 => 'Security checks on virtual account servers and replacement of aging servers.';
+
+  @override
+  String get cvHectoFinancial4 => 'Installed new services on virtual account servers and trained government officials.';
+
+  @override
+  String get cvHectoFinancial5 => 'Answered phone CS for virtual account payment service.';
+
+  @override
+  String get cvUniversity1 => 'Korea University';
+
+  @override
+  String get cvUniversity2 => 'Mechatronics Major (Master\'s Dropout), General Graduate School';
+
+  @override
+  String get cvUniversity3 => 'Control and Instrumentation Engineering major (B.S.), College of Science and Technology';
 }
