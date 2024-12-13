836b895f1dd6c9dd50ac64f654c2935105971e4a
HwangSooHyeon <HwangSooHyeon@users.noreply.github.com>
2024-12-13 03:14:23 +0000

    deploy: d351cf7a0da82a37a7a57f4ab34007ebf91041f5

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "282289094"
+    serviceWorkerVersion: "1766927674"
   }
 });
