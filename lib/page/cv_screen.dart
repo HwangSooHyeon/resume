ff97065aaff076414b1ddaf0047804753ab7cfea
hwangsoohyeon <mikaelh1651@gmail.com>
2025-03-31 16:16:19 +0900

    feat: complete first version of cv

--- lib/page/cv_screen.dart
+++ lib/page/cv_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:resume/model/enum/cv_enum.dart';
 import 'package:resume/util/extension/build_context_extension.dart';
-import 'package:resume/page/cv/widget/timeline.dart';
+import 'package:resume/page/cv/widget/timeline/timeline.dart';
 import 'package:resume/util/theme/custom_text_theme.dart';
 
 class CvScreen extends StatefulWidget {
@@ -20,8 +20,8 @@ class _CvScreenState extends State<CvScreen> with TickerProviderStateMixin {
     vsync: this,
   );
   late final _offsetAnimation = Tween(
-    begin: Offset(1, 0),
-    end: Offset(0, 0.0),
+    begin: const Offset(0, 1),
+    end: const Offset(0, 0.0),
   ).animate(
     CurvedAnimation(
       parent: _animationController,
@@ -52,7 +52,7 @@ class _CvScreenState extends State<CvScreen> with TickerProviderStateMixin {
       child: Wrap(
         runSpacing: 30,
         children: [
-          Container(
+          SizedBox(
             width: context.width - 200,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -62,7 +62,7 @@ class _CvScreenState extends State<CvScreen> with TickerProviderStateMixin {
                   style: CustomTextTheme.boldTextStyle(fontSize: 32),
                 ),
                 const Gap(20),
-                Timeline(
+                Timeline.defaultDate(
                   timelines: [
                     TimelineModel(
                       startDate: DateTime(2020),
@@ -89,6 +89,11 @@ class _CvScreenState extends State<CvScreen> with TickerProviderStateMixin {
                       endDate: DateTime(2025),
                       label: '2024',
                     ),
+                    TimelineModel(
+                      startDate: DateTime(2025),
+                      endDate: DateTime(2026),
+                      label: '2025',
+                    ),
                   ],
                 ),
                 const Gap(10),
@@ -96,48 +101,73 @@ class _CvScreenState extends State<CvScreen> with TickerProviderStateMixin {
                   timelines: [
                     TimelineModel(
                       startDate: DateTime(2023, 4),
-                      endDate: DateTime(2024, 12),
-                      color: Colors.blueAccent,
-                      label: '디렉셔널',
-                      toolTip: 'directional',
+                      endDate: DateTime(2025, 4),
+                      label: context.locale!.cvDirectionalTitle,
+                      onTap: () {
+                        setState(() {
+                          _currentScreen = CvEnum.directional.widget;
+                        });
+                      },
                     ),
                     TimelineModel(
                       startDate: DateTime(2022, 10),
                       endDate: DateTime(2022, 12),
-                      color: Colors.lightBlue,
-                      label: '우아한테크코스',
-                      toolTip: 'woowa',
+                      label: context.locale!.cvWoowaTitle,
+                      onTap: () {
+                        setState(() {
+                          _currentScreen = CvEnum.woowa.widget;
+                        });
+                      },
                     ),
                     TimelineModel(
                       startDate: DateTime(2021, 11),
-                      endDate: DateTime(2022, 9),
-                      color: Colors.yellow[900]!,
-                      label: '헥토 파이낸셜',
-                      toolTip: 'hecto',
+                      endDate: DateTime(2022, 8),
+                      label: context.locale!.cvHectoFinancialTitle,
+                      onTap: () {
+                        setState(() {
+                          _currentScreen = CvEnum.hecto.widget;
+                        });
+                      },
                     ),
                     TimelineModel(
                       startDate: DateTime(2020, 12),
                       endDate: DateTime(2021, 6),
-                      color: Colors.green,
-                      label: 'IT 교육',
-                      toolTip: 'IT Academy',
+                      label: context.locale!.cvItAcademyTitle,
+                      onTap: () {
+                        setState(() {
+                          _currentScreen = CvEnum.itAcademy.widget;
+                        });
+                      },
+                    ),
+                    TimelineModel(
+                      startDate: DateTime(2020, 3),
+                      endDate: DateTime(2020, 9),
+                      label: context.locale!.cvGraduateSchoolTitle,
+                      onTap: () {
+                        setState(() {
+                          _currentScreen = CvEnum.graduateSchool.widget;
+                        });
+                      },
                     ),
                     TimelineModel(
                       startDate: DateTime(2020, 1),
-                      endDate: DateTime(2020, 9),
-                      color: Color(0xffDC143C),
-                      label: '대학원',
-                      toolTip: 'graduate school',
+                      endDate: DateTime(2020, 2),
+                      label: context.locale!.cvUniversityTitle,
+                      onTap: () {
+                        setState(() {
+                          _currentScreen = CvEnum.university.widget;
+                        });
+                      },
                     ),
                   ],
                 ),
+                const Gap(4),
                 Timeline(
                   timelines: [
                     TimelineModel(
                       startDate: DateTime(2023, 8),
                       endDate: DateTime(2023, 12),
-                      color: Color(0xffDC143C),
-                      label: context.locale!.cvSbl,
+                      label: context.locale!.cvSblTitle,
                       onTap: () {
                         setState(() {
                           _currentScreen = CvEnum.sbl.widget;
@@ -147,33 +177,52 @@ class _CvScreenState extends State<CvScreen> with TickerProviderStateMixin {
                     TimelineModel(
                       startDate: DateTime(2024, 6),
                       endDate: DateTime(2024, 11),
-                      color: Color(0xffDC143C),
-                      label: context.locale!.cvFireAnt,
+                      label: context.locale!.cvFireAntTitle,
                       onTap: () {
                         setState(() {
                           _currentScreen = CvEnum.fireAnt.widget;
                         });
                       },
                     ),
+                    TimelineModel(
+                      startDate: DateTime(2024, 12),
+                      endDate: DateTime(2025, 4),
+                      label: context.locale!.cvMoneytoringTitle,
+                      onTap: () {
+                        setState(() {
+                          _currentScreen = CvEnum.moneytoring.widget;
+                        });
+                      },
+                    ),
                   ],
                 ),
+                const Gap(4),
                 Timeline(
                   timelines: [
                     TimelineModel(
                       startDate: DateTime(2024, 5),
-                      endDate: DateTime(2024, 7),
-                      color: Color(0xffDC143C),
-                      label: context.locale!.cvInvestarBackOffice,
+                      endDate: DateTime(2024, 8),
+                      label: context.locale!.cvInvestarBackOfficeTitle,
+                      onTap: () {
+                        setState(() {
+                          _currentScreen = CvEnum.investarBackOffice.widget;
+                        });
+                      },
                     ),
                   ],
                 ),
+                const Gap(4),
                 Timeline(
                   timelines: [
                     TimelineModel(
                       startDate: DateTime(2023, 4),
                       endDate: DateTime(2024, 11),
-                      color: Color(0xffDC143C),
-                      label: context.locale!.cvInvestar,
+                      label: context.locale!.cvInvestarTitle,
+                      onTap: () {
+                        setState(() {
+                          _currentScreen = CvEnum.investar.widget;
+                        });
+                      },
                     ),
                   ],
                 ),
@@ -184,7 +233,7 @@ class _CvScreenState extends State<CvScreen> with TickerProviderStateMixin {
             children: [
               SizedBox(
                 width: context.width - 200,
-                height: context.height - 184 - 18 - 250 - 30,
+                height: context.height - 184 - 18 - 250 - 40,
                 child: SlideTransition(
                   position: _offsetAnimation,
                   child: _currentScreen,
