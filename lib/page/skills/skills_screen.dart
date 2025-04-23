b554c79d1ff62ec67b952fc38f0fb353a8f6aef2
hwangsoohyeon <mikaelh1651@gmail.com>
2025-04-23 20:02:26 +0900

    hotfix: fix clipBehavior and refactor codes

--- lib/page/skills/skills_screen.dart
+++ lib/page/skills/skills_screen.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:resume/page/skills/screen/skills_library_screen.dart';
 import 'package:resume/page/skills/screen/skills_programming_language_screen.dart';
@@ -7,6 +8,7 @@ import 'package:resume/util/extension/build_context_extension.dart';
 import 'package:resume/util/function/duration.dart';
 import 'package:resume/util/theme/custom_text_theme.dart';
 import 'package:resume/widget/common/fade_slide_widget.dart';
+import 'package:resume/widget/common/hover_box_widget.dart';
 
 class SkillsScreen extends StatefulWidget {
   static const String path = '/skills';
@@ -69,49 +71,29 @@ class _SkillsScreenState extends State<SkillsScreen>
               margin: const EdgeInsets.only(bottom: 32.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
-                children: _tabs.asMap().entries.map((entry) {
-                  final int index = entry.key;
-                  final String tab = entry.value;
-                  final bool isSelected = _tabController.index == index;
-                  return MouseRegion(
-                    cursor: SystemMouseCursors.click,
-                    child: GestureDetector(
-                      onTap: () {
-                        _tabController.animateTo(index);
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.only(right: 16.0),
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 24.0, vertical: 12.0),
-                        decoration: BoxDecoration(
-                          color: isSelected
-                              ? context.colorScheme.primary
-                              : context.colorScheme.surface,
-                          borderRadius: BorderRadius.circular(30),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black
-                                  .withValues(alpha: isSelected ? 0.16 : 0.08),
-                              offset: const Offset(2, 4),
-                              blurRadius: 20,
-                              spreadRadius: 0,
+                children: _tabs.mapIndexed((i, e) {
+                  final bool isSelected = _tabController.index == i;
+                  return HoverBoxWidget(
+                    onTap: () => _tabController.animateTo(i),
+                    margin: const EdgeInsets.only(right: 16.0),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 24.0, vertical: 12.0),
+                    backgroundColor: isSelected
+                        ? context.colorScheme.primary
+                        : context.colorScheme.surface,
+                    borderRadius: 32,
+                    child: Text(
+                      e,
+                      style: isSelected
+                          ? CustomTextTheme.boldTextStyle(
+                              fontSize: 16,
+                              color: context.colorScheme.onPrimary,
+                            )
+                          : CustomTextTheme.regularTextStyle(
+                              fontSize: 16,
+                              color: context.colorScheme.onSurface
+                                  .withValues(alpha: 0.8),
                             ),
-                          ],
-                        ),
-                        child: Text(
-                          tab,
-                          style: isSelected
-                              ? CustomTextTheme.boldTextStyle(
-                                  fontSize: 16,
-                                  color: context.colorScheme.onPrimary,
-                                )
-                              : CustomTextTheme.regularTextStyle(
-                                  fontSize: 16,
-                                  color: context.colorScheme.onSurface
-                                      .withValues(alpha: 0.8),
-                                ),
-                        ),
-                      ),
                     ),
                   );
                 }).toList(),
@@ -120,6 +102,7 @@ class _SkillsScreenState extends State<SkillsScreen>
             Expanded(
               child: TabBarView(
                 controller: _tabController,
+                clipBehavior: Clip.none,
                 children: const [
                   SkillsProgrammingLanguageScreen(),
                   SkillsLibraryScreen(),
