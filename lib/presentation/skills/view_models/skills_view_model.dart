0556fa24c82a9d424697e63b538316fc697f2656
hwangsoohyeon <mikaelh1651@gmail.com>
2025-04-25 02:32:17 +0900

    chore: remove unused import

--- lib/presentation/skills/view_models/skills_view_model.dart
+++ lib/presentation/skills/view_models/skills_view_model.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:resume/core/extensions/locale_extension.dart';
 import 'package:resume/domain/entities/skills_model.dart';
