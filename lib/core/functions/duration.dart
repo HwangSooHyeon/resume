8c6279c4e111758c60946196c995c7880820804a
hwangsoohyeon <mikaelh1651@gmail.com>
2025-04-24 15:07:47 +0900

    feat: change delay time of animation delay func

--- lib/core/functions/duration.dart
+++ lib/core/functions/duration.dart
@@ -2,5 +2,5 @@ Duration animationDelay({
   required int order,
 }) =>
     Duration(
-      milliseconds: 300 + (order * 120).clamp(0, 3000),
+      milliseconds: 300 + (order * 240).clamp(0, 3000),
     );
